42810655d5199756a4041ac37adafa42ba80fc8b
Paul Bilnoski <bilnoski@tamu.edu>
2014-06-10 11:48:21 -0500

    Add initial testing for accounts and JAAS login

---
+++ tests/edu.tamu.tcat.oss.account.test/src/edu/tamu/tcat/oss/account/test/DatabaseLoginModule.java
@@ -0,0 +1,216 @@
+package edu.tamu.tcat.oss.account.test;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.login.LoginException;
+import javax.security.auth.spi.LoginModule;
+
+//TODO: get JAAS authn example working against database
+//      write tcat.oss.account wrapper for use with REST
+//        Token doLogin(String u, String p);
+//        void doLogout(Token t);
+//        String getUserInfo(String? key, Token t);
+//          back by OSGI service that can cache user subj/principles, flush on logout or on token expiration
+//        boolean hasPerm(String? perm, Token t);
+//          back by OSGI service that can crawl user/group space and build cache to be flushed on live perm change
+//      token: userid, exp time, ip addr, server secret, plus sha256 of uid+exp+ip+secret
+//      not exposing Subject and Principals until needed
+//      token returned in header, to be update in subsequent requests with moving timeout window
+
+// use pbkdf2impl for password storage, start with 10k rounds and calibrate
+// use securetokenimpl for token generation and processing
+public class DatabaseLoginModule implements LoginModule
+{
+   private static final Logger debug = Logger.getLogger(DatabaseLoginModule.class.getName());
+   private Subject subject;
+   private CallbackHandler cbh;
+   
+   private boolean didLogin = false;
+   private boolean didCommit = false;
+   
+   // Store
+   private String userName;
+   private DatabasePrincipal dbpUserName;
+   
+   static class DatabasePrincipal implements Principal
+   {
+      private final String name;
+      
+      public DatabasePrincipal(String name)
+      {
+         Objects.requireNonNull(name);
+         this.name = name;
+      }
+      
+      @Override
+      public String getName()
+      {
+         return name;
+      }
+      
+      @Override
+      public String toString()
+      {
+         return "TcatPrincipal["+name+"]";
+      }
+
+      @Override
+      public int hashCode()
+      {
+         return name.hashCode();
+      }
+
+      @Override
+      public boolean equals(Object obj)
+      {
+         if (this == obj)
+            return true;
+         if (obj instanceof DatabasePrincipal)
+         {
+            DatabasePrincipal other = (DatabasePrincipal)obj;
+            if (!name.equals(other.name))
+               return false;
+         }
+         return true;
+      }
+   }
+   
+   public DatabaseLoginModule()
+   {
+      debug.info("created!");
+   }
+   
+   @Override
+   public void initialize(Subject subj, CallbackHandler callbackHandler, Map<String, ?> sharedState, Map<String, ?> options)
+   {
+      this.subject = subj;
+      this.cbh = callbackHandler;
+      debug.info("init");
+   }
+
+   @Override
+   public boolean login() throws LoginException
+   {
+      debug.info("login");
+      
+      String name = null;
+      try
+      {
+         List<Callback> cbs = new ArrayList<>();
+         // There are a few library-defined callbacks commonly used
+         cbs.add(new NameCallback("gimme name!"));
+         cbh.handle(cbs.toArray(new Callback[cbs.size()]));
+         
+         name = ((NameCallback)cbs.get(0)).getName();
+      }
+      catch (UnsupportedCallbackException e)
+      {
+         throw new LoginException("Callback handler does not support callback: " + e.getCallback());
+      }
+      catch (IOException e)
+      {
+         throw new IllegalStateException("Failed handling callbacks", e);
+      }
+      
+      boolean succ = false;
+      
+      // validate credential
+      if (name != null && !name.trim().isEmpty())
+      {
+         succ = true;
+      }
+      
+      // if not successful
+      if (!succ)
+      {
+         didLogin = false;
+         return false;
+      }
+      
+      // Store the data in fields to be used in commit()
+      userName = name;
+      
+      didLogin = true;
+      return true;
+   }
+
+   @Override
+   public boolean commit() throws LoginException
+   {
+      debug.info("commit");
+      
+      // login did not succeed - clean up internal state
+      if (!didLogin)
+         return false;
+      
+      // Store principal as internal state independent from username string
+      dbpUserName = new DatabasePrincipal(userName);
+      
+      Set<Principal> princs = subject.getPrincipals();
+      // principals compare using .equals
+      if (!princs.contains(dbpUserName))
+         princs.add(dbpUserName);
+      
+      // clean up state
+      // no need to retain username since a principal now exists
+      userName = null;
+      
+      didCommit = true;
+      return true;
+   }
+
+   @Override
+   public boolean abort() throws LoginException
+   {
+      debug.info("abort");
+      
+      // no state to clean up for failed login
+      if (!didLogin)
+         return false;
+      
+      // login success, commit failed
+      if (!didCommit)
+      {
+         userName = null;
+         return true;
+      }
+      
+      // login and commit succeeded, but some other login module failed, so clean up state
+      logout();
+      
+      return true;
+   }
+
+   @Override
+   public boolean logout() throws LoginException
+   {
+      debug.info("logout");
+      
+      // remove principals added, which will compare via .equals
+      Set<Principal> princs = subject.getPrincipals();
+      // Allow logout to succeed if login failed, so check for state
+      if (dbpUserName != null)
+         princs.remove(dbpUserName);
+      
+      didLogin = false;
+      didCommit = false;
+      
+      // Clean up state
+      userName = null;
+      dbpUserName = null;
+      
+      return true;
+   }
+}
