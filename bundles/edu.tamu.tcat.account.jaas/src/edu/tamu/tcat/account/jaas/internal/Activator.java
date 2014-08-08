7dd88284e1fcec5d0df3c5aab697b2a3811edd27
Paul Bilnoski <bilnoski@tamu.edu>
2014-08-08 11:16:41 -0500

    Add new bundle to handle OSGI-enabled JAAS

---
+++ bundles/edu.tamu.tcat.account.jaas/src/edu/tamu/tcat/account/jaas/internal/Activator.java
@@ -0,0 +1,18 @@
+package edu.tamu.tcat.account.jaas.internal;
+
+import edu.tamu.tcat.osgi.services.util.ActivatorBase;
+
+public class Activator extends ActivatorBase
+{
+   private static Activator instance;
+
+   public Activator()
+   {
+      instance = this;
+   }
+
+   public static Activator getDefault()
+   {
+      return instance;
+   }
+}
