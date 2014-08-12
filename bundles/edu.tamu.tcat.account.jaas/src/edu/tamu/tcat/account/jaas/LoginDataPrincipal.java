c9f1361d064b8346943e35412caf29be644d9a93
Paul Bilnoski <bilnoski@tamu.edu>
2014-08-12 09:44:05 -0500

    Clean up deps for OSGI login module wrapper use

--- bundles/edu.tamu.tcat.account.jaas/src/edu/tamu/tcat/account/jaas/LoginDataPrincipal.java
+++ bundles/edu.tamu.tcat.account.jaas/src/edu/tamu/tcat/account/jaas/LoginDataPrincipal.java
@@ -7,10 +7,10 @@ import javax.security.auth.spi.LoginModule;
 import edu.tamu.tcat.account.login.LoginData;
 
 /**
- * A JAAS {@link Principal} that provides access to a {@link LoginData}, allowing the
- * {@link LoginModule} to construct one from its accessible data.
+ * A JAAS {@link Principal} that provides access to a {@link LoginData}, allowing a
+ * {@link LoginModule} to expose one through the JAAS API.
  * <p>
- * If one of these is needed an a <tt>LoginModule</tt> does not provide one,
+ * If one of these is needed and a <tt>LoginModule</tt> does not provide one,
  * the caller will need to construct a <tt>LoginData</tt> from
  * the <tt>Principal</tt>s known to be provided by that <tt>LoginModule</tt>.
  */
