e3e4842c911f06e3bee2f5e75f21178eceede681
Graham Booker <gbooker@tamu.edu>
2014-11-19 16:40:14 -0600

    Replaced comments with thrown exceptions and responses.

--- bundles/edu.tamu.tcat.account.jaxrs/src/edu/tamu/tcat/account/jaxrs/provider/token/TokenSecurityObjectFilter.java
+++ bundles/edu.tamu.tcat.account.jaxrs/src/edu/tamu/tcat/account/jaxrs/provider/token/TokenSecurityObjectFilter.java
@@ -17,8 +17,11 @@ package edu.tamu.tcat.account.jaxrs.provider.token;
 
 import java.io.IOException;
 
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import edu.tamu.tcat.account.jaxrs.bean.ContextBean;
 import edu.tamu.tcat.account.token.TokenService;
@@ -35,25 +38,23 @@ public class TokenSecurityObjectFilter<PayloadType> implements ContainerRequestF
    @Override
    public void filter(ContainerRequestContext requestContext) throws IOException
    {
-//      try
-//      {
-         String tokenHeader = requestContext.getHeaderString("Token");
-//         if (tokenHeader == null)
-//            throw new BadEPSSRequestException("Token not provided to service requiring token\n");
-         try
-         {
-            PayloadType tokenPayload = tokenService.unpackToken(tokenHeader);
-            ContextBean.from(requestContext).install(tokenService.getPayloadType()).set(tokenPayload);
-         }
-         catch (Exception e)
-         {
-//            //The token was invalid.
-//            throw new BadEPSSRequestException("Invalid token provided\n");
-         }
-//      }
-//      catch (BadEPSSRequestException e)
-//      {
-//         throw BadEPSSRequestExceptionMapper.createException(e);
-//      }
+      String tokenHeader = requestContext.getHeaderString("Token");
+      if (tokenHeader == null)
+         throw new BadRequestException(Response.status(Response.Status.BAD_REQUEST)
+               .entity("Token not provided to service requiring token\n")
+               .type(MediaType.TEXT_PLAIN)
+               .build());
+      try
+      {
+         PayloadType tokenPayload = tokenService.unpackToken(tokenHeader);
+         ContextBean.from(requestContext).install(tokenService.getPayloadType()).set(tokenPayload);
+      }
+      catch (Exception e)
+      {
+         throw new BadRequestException(Response.status(Response.Status.BAD_REQUEST)
+               .entity("Invalid token provided\n")
+               .type(MediaType.TEXT_PLAIN)
+               .build());
+      }
    }
 }
