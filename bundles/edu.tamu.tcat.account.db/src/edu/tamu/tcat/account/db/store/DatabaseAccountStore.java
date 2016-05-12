e06be1febed8335b4cf3c5d4629a48f10268c7a1
Paul Bilnoski <bilnoski@tamu.edu>
2016-05-12 17:45:28 -0500

    Fix old comment

--- bundles/edu.tamu.tcat.account.db/src/edu/tamu/tcat/account/db/store/DatabaseAccountStore.java
+++ bundles/edu.tamu.tcat.account.db/src/edu/tamu/tcat/account/db/store/DatabaseAccountStore.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -46,7 +46,7 @@ import edu.tamu.tcat.db.exec.sql.SqlExecutor;
  *   CONSTRAINT id PRIMARY KEY (id)
  * )
  * </pre>
- * 
+ *
  * Where:
  * <ul><li><em>id</em> is a per-record identifier</li>
  *     <li><em>account_id</em> is the string form of a {@link UUID} (unique)</li>
@@ -66,53 +66,51 @@ public class DatabaseAccountStore implements AccountStore
       public String title;
       public String type;
       public boolean isActive;
-      
+
       public DatabaseAccount()
       {
       }
-      
+
       @Override
       public UUID getId()
       {
          return uuid;
       }
-      
+
       @Override
       public String getTitle()
       {
          return title;
       }
-      
+
       public String getType()
       {
          return type;
       }
-      
+
       @Override
       public boolean isActive()
       {
          return isActive;
       }
    }
-   
+
    private SqlExecutor dbExec;
-   
+
    public void bind(SqlExecutor db)
    {
       dbExec = db;
    }
-   
+
    /*
     * Table "account": stores accounts, groups, and roles
     * Table "account_authn": maps accounts that can log in to login provider and login-provider-user-id
     * Table "authn_local": is a "local db" authn provider using "long" for user-id
-    * 
+    *
     * TODO: create groups for "everyone" and "debug"
-    * TODO: create accounts for paul, neal, jesse, matthew
-    *       create "account PUT" in the resource, perhaps?
     * TODO: link accounts to local auth in account_authn
     */
-   
+
    @Override
    public Account lookup(LoginData loginData) throws AccountException
    {
@@ -123,29 +121,29 @@ public class DatabaseAccountStore implements AccountStore
       final String puid = loginData.getLoginUserId();
       if (puid == null || puid.trim().isEmpty())
          throw new IllegalArgumentException("Login data provider user id may not be empty");
-      
+
       SqlExecutor.ExecutorTask<DatabaseAccount> task = new SqlExecutor.ExecutorTask<DatabaseAccount>()
       {
          @Override
          public DatabaseAccount execute(Connection conn) throws Exception
          {
             UUID uuid = null;
-            
+
             String sql = "SELECT account_id FROM account_authn WHERE auth_provider_user_id = ? AND auth_provider_id = ?";
             try (PreparedStatement ps = conn.prepareStatement(sql))
             {
                ps.setString(1, puid);
                ps.setString(2, pid);
-               
+
                try (ResultSet rs = ps.executeQuery())
                {
                   if (!rs.next())
                      throw new AccountNotFoundException("No mapping exists for provider ["+pid+"] and user ["+puid+"]");
-                  
+
                   String uuidStr = rs.getString("account_id");
                   if (uuidStr == null || uuidStr.trim().isEmpty())
                      throw new IllegalStateException("Mapping for provider ["+pid+"] and user ["+puid+"] has missing account id");
-                  
+
                   try
                   {
                      uuid = UUID.fromString(uuidStr);
@@ -156,22 +154,22 @@ public class DatabaseAccountStore implements AccountStore
                   }
                }
             }
-            
+
             // Have a valid UUID, now find the account data for it
             // This could be done with a join and more efficient query, but it may be helpful to have the extra logging
             sql = "SELECT * FROM account WHERE account_id = ?";
             try (PreparedStatement ps = conn.prepareStatement(sql))
             {
                ps.setString(1, uuid.toString());
-               
+
                try (ResultSet rs = ps.executeQuery())
                {
                   if (!rs.next())
                      throw new AccountNotFoundException("No account exists for id ["+uuid+"] from provider ["+pid+"] and user ["+puid+"]");
-                  
+
                   if (rs.getBoolean("is_deleted"))
                      throw new AccountNotFoundException("Cannot retrieve deleted account ["+uuid+"] from provider ["+pid+"] and user ["+puid+"]");
-                  
+
                   DatabaseAccount rv = new DatabaseAccount();
                   rv.id = rs.getLong("id");
                   rv.uuid = uuid;
@@ -179,13 +177,13 @@ public class DatabaseAccountStore implements AccountStore
                   // For now, all accounts are "user"
                   //rv.type = rs.getString("type");
                   rv.isActive = rs.getBoolean("is_active");
-                  
+
                   return rv;
                }
             }
          }
       };
-      
+
       try
       {
          Future<DatabaseAccount> f = dbExec.submit(task);
@@ -205,26 +203,26 @@ public class DatabaseAccountStore implements AccountStore
       // TODO Auto-generated method stub
       return null;
    }
-   
+
    public Account createAccount(String title) throws AccountException
    {
       if (title == null || title.trim().isEmpty())
          throw new IllegalArgumentException("Account title may not be empty");
-      
+
       DatabaseAccount newAcct = new DatabaseAccount();
       newAcct.uuid = UUID.randomUUID();
       newAcct.title = title;
       newAcct.type = "user";
-      
+
       final AtomicReference<DatabaseAccount> ref = new AtomicReference<>(newAcct);
-      
+
       SqlExecutor.ExecutorTask<DatabaseAccount> task = new SqlExecutor.ExecutorTask<DatabaseAccount>()
       {
          @Override
          public DatabaseAccount execute(Connection conn) throws Exception
          {
             DatabaseAccount acct = ref.get();
-            
+
             String sql = "INSERT INTO account (account_id, title, account_type, is_active, is_deleted) VALUES (?,?,?,?,?)";
             try (PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS))
             {
@@ -233,22 +231,22 @@ public class DatabaseAccountStore implements AccountStore
                ps.setString(3, acct.type);
                ps.setBoolean(4, false); // new accounts are not active
                ps.setBoolean(5, false); // new accounts are not deleted
-               
+
                ps.execute();
                try (ResultSet rs = ps.getGeneratedKeys())
                {
                   if (!rs.next())
                      throw new IllegalStateException("Failed sql insert providing new key");
-                  
+
                   acct.id = rs.getLong("id");
                }
-               
+
                ref.set(acct);
                return acct;
             }
          }
       };
-      
+
       try
       {
          Future<? extends Account> f = dbExec.submit(task);
