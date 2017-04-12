1e5e5add9e626df7939054d25b97f63df06ee121
Blake Stoker <blake-stoker@tamu.edu>
2017-04-12 15:07:27 -0500

    move AD Object guid logic to lib bundle

--- bundles/edu.tamu.tcat.account.apacheds.ad.login/src/edu/tamu/tcat/account/apacheds/ad/login/LdapLoginProvider.java
+++ bundles/edu.tamu.tcat.account.apacheds.ad.login/src/edu/tamu/tcat/account/apacheds/ad/login/LdapLoginProvider.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import edu.tamu.tcat.account.AccountException;
+import edu.tamu.tcat.account.apacheds.ADObjectGUIDConverter;
 import edu.tamu.tcat.account.apacheds.LdapAuthException;
 import edu.tamu.tcat.account.apacheds.LdapException;
 import edu.tamu.tcat.account.apacheds.LdapHelperReader;
@@ -179,56 +180,6 @@ public class LdapLoginProvider implements LoginProvider
       }
    }
 
-   /**
-    * Converts a GUID byte[] as retrieved from MS AD/LDS to a readable GUID value
-    * of the format "xxxx-xx-xx-xx-xxxxxx"
-    */
-   public static String toGuidString(byte[] objectGUID)
-   {
-      StringBuilder uuidStr = new StringBuilder();
-
-      int[] positions = {3,2,1,0, -1, 5,4, -1, 7,6, -1, 8,9, -1, 10,11,12,13,14,15};
-
-      for (int pos : positions)
-      {
-         if (pos < 0)
-         {
-            uuidStr.append('-');
-         }
-         else
-         {
-            int b = (int)objectGUID[pos] & 0xFF;
-            if (b <= 0xF)
-               uuidStr.append('0');
-            uuidStr.append(Integer.toHexString(b));
-         }
-      }
-
-      return uuidStr.toString();
-   }
-
-   /**
-    * Converts a GUID byte[] as retrieved from MS AD/LDS to a query key
-    * of the format having with 16 "\xx" hex segments.
-    * This value may be used in an LDAP query such as:
-    * (objectGUID=\01\01\01\01\01\01\01\01\01\01\01\01\01\01\01\01)
-    */
-   public static String toByteString(byte[] objectGUID)
-   {
-      StringBuilder out = new StringBuilder();
-
-      for (byte b : objectGUID)
-      {
-         out.append('\\');
-         int v = (int)b & 0xFF;
-         if (v <= 0xF)
-            out.append('0');
-         out.append(Integer.toHexString(v));
-      }
-
-      return out.toString();
-   }
-
    private static class LdapUserData implements LoginData
    {
       // these should be somewhere external
@@ -278,7 +229,7 @@ public class LdapLoginProvider implements LoginProvider
                .collect(Collectors.toList());
          guid = (byte[])helper.getAttributes(dn, "objectGUID").stream().findFirst().orElse(null);
          // The user-id contains two parts; the first is the readable GUID, then a semicolon, then the byte string used for LDAP queries
-         userId = toGuidString(guid) + ";" + toByteString(guid);
+         userId = ADObjectGUIDConverter.toGuidString(guid) + ";" + ADObjectGUIDConverter.toByteString(guid);
       }
 
       @Override
